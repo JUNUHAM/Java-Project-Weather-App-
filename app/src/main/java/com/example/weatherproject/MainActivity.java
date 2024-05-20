01b646fad989560b159d191e1957e038621a9ff1
JUNUHAM <langeoda@gmail.com>
2024-05-20 22:19:00 +0900

    Spinner 구현

--- app/src/main/java/com/example/weatherproject/MainActivity.java
+++ app/src/main/java/com/example/weatherproject/MainActivity.java
@@ -5,6 +5,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,11 +15,32 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView cityName;
+
+    String[] itmes = {"서울", "인천", "대전", "대구", "울산", "부산", "광주", "안양"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        cityName = findViewById(R.id.cityName);
+        Spinner spinner = findViewById(R.id.my_spinner);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, itmes);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                cityName.setText(itmes[position]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                cityName.setText("도시 선택");
+            }
+        });
     }
 }
